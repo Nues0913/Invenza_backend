4a331d29b3c17138cd0a73927490f6d9e0d44164
Nues0913 <a09871530274@gmail.com>
2025-05-21 00:51:50 +0800

    fix testUser encrypt problem, adjust layout

--- src/main/java/com/example/invenza/config/SecurityConfig.java
+++ src/main/java/com/example/invenza/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,23 +26,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    // @Autowired 
-    // private UserDetailsService userDetailsService;
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService) {
-        return new JwtAuthenticationFilter(jwtService);
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth ->
                 auth.requestMatchers(
-                    "/login",               // 用戶登入
-                    "/test",                // 測試用
-                    "/forgotpassword"       // 忘記密碼
+                    "/api/auth/login",
+                    "/api/auth/test",
+                    "/api/auth/forgotpassword"
                 ).permitAll()
                 .anyRequest().authenticated() // 其他請求需要身份驗證
             )
@@ -54,11 +46,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         var member1 = new Member();
         member1.setId("1");
         member1.setUsername("user1");
-        member1.setPassword("$2a$10$wH8Qw1Qw1Qw1Qw1Qw1Qw1uQw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Q");
+        member1.setPassword(passwordEncoder.encode("111"));
         member1.setEmail("user1@email.com");
         member1.setPhone("0912345678");
         return new UserDetailsServiceImpl(List.of(member1));
@@ -70,8 +62,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class).build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService) {
+        return new JwtAuthenticationFilter(jwtService);
     }
 
     @Bean
