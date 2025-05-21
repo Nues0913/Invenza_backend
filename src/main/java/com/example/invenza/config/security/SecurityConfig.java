3579977668ddc3060db00a576dabd374219c9542
Nues0913 <a09871530274@gmail.com>
2025-05-21 22:14:16 +0800

    refactor code

--- src/main/java/com/example/invenza/config/security/SecurityConfig.java
+++ src/main/java/com/example/invenza/config/security/SecurityConfig.java
@@ -1,23 +1,17 @@
-package com.example.invenza.config;
-
-import java.util.List;
+package com.example.invenza.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import com.example.invenza.entity.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,38 +21,8 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter) throws Exception {
-        return http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth ->
-                auth.requestMatchers(
-                    "/api/auth/login",
-                    "/api/auth/test",
-                    "/api/auth/forgotpassword"
-                ).permitAll()
-                .anyRequest().authenticated() // 其他請求需要身份驗證
-            )
-            .sessionManagement(sessionManagement -> {
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-            })
-            .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class)
-            .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        var member1 = new Member();
-        member1.setId("1");
-        member1.setUsername("user1");
-        member1.setPassword(passwordEncoder.encode("111"));
-        member1.setEmail("user1@email.com");
-        member1.setPhone("0912345678");
-        return new UserDetailsServiceImpl(List.of(member1));
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService) {
+        return new JwtAuthenticationFilter(jwtService);
     }
 
     @Bean
@@ -67,15 +31,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtService jwtService) {
-        return new JwtAuthenticationFilter(jwtService);
-    }
-
-    @Bean
     public JwtService jwtService(
             @Value("${jwt.secret-key}") String secretKeyStr,
             @Value("${jwt.valid-seconds}") int validSeconds
     ) {
         return new JwtService(secretKeyStr, validSeconds);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests(auth ->
+                auth.requestMatchers(
+                    "/api/auth/login",
+                    "/api/auth/test",
+                    "/api/auth/forgot-password"
+                ).permitAll()
+                .anyRequest().authenticated() // 其他請求需要身份驗證
+            )
+            .sessionManagement(sessionManagement -> {
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            })
+            .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class)
+            .build();
     }
 }
