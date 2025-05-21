95cac6950f7df4b5deaede5039af8e1b97badbd5
Nues0913 <a09871530274@gmail.com>
2025-05-21 22:20:05 +0800

    adapt with repository

--- src/main/java/com/example/invenza/entity/Member.java
+++ src/main/java/com/example/invenza/entity/Member.java
@@ -1,13 +1,20 @@
 package com.example.invenza.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "member")
 public class Member {
+    @Id
     private String id;
-    private String username;
+    private String account;
+    private String name;
     private String password;
     private String email;
     private String phone;
