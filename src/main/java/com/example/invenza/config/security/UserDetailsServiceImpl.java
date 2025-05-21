c86b5690de2409b9c5ec17e006376fc8f02c2b3f
Nues0913 <a09871530274@gmail.com>
2025-05-21 22:16:42 +0800

    request data from repository

---
+++ src/main/java/com/example/invenza/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.invenza.config.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.invenza.repository.MemberRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public UserDetailsServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String account) throws UsernameNotFoundException {
+        var member = memberRepository.findByAccount(account);
+        if (member == null) {
+            throw new UsernameNotFoundException("Can't find user: " + account);
+        }
+        return new MemberUserDetails(member);
+    }
+}
