ca7c476713fa9385abd22c5507e63c4556ffa4e3
Nues0913 <a09871530274@gmail.com>
2025-06-22 08:13:18 +0800

    新增inventory-get-data params功能

--- src/main/java/com/example/invenza/repository/CommodityRepository.java
+++ src/main/java/com/example/invenza/repository/CommodityRepository.java
@@ -3,8 +3,9 @@ package com.example.invenza.repository;
 import java.util.List;
 import com.example.invenza.entity.Commodity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CommodityRepository extends JpaRepository<Commodity, Long> {
+public interface CommodityRepository extends JpaRepository<Commodity, Long>, JpaSpecificationExecutor<Commodity> {
     Commodity findByName(String name);
     Commodity findByType(String type);
     List<Commodity> findByStockQuantityLessThan(double quantity);
